91d6bc1dac532136138e97da88a9c59eefcbb698
mnmlyn <mnmlyn@163.com>
2019-02-02 18:23:44 +0800

    使用okHttp和AsyncTask，发起HTTP请求，依据请求结果，更新UI

--- app/src/main/java/com/example/min/myappagain/Frag2Fragment.java
+++ app/src/main/java/com/example/min/myappagain/Frag2Fragment.java
@@ -1,17 +1,65 @@
 package com.example.min.myappagain;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Frag2Fragment extends Fragment {
+    private View frag2Layout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.frag2, container, false);
+        frag2Layout = inflater.inflate(R.layout.frag2, container, false);
+        AsyncGetData asyncGetData = new AsyncGetData();
+        asyncGetData.execute();
+        return frag2Layout;
     }
+
+    public class AsyncGetData extends AsyncTask<Void, Void ,String>{
+        @Override
+        protected String doInBackground(Void... voids) {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.5:8888/index1.html")
+                    .get()
+                    .build();
+            Response response = null;
+            try{
+                response = okHttpClient.newCall(request).execute();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(response != null && response.isSuccessful()){
+                assert response.body() != null;
+                try {
+                    return response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+            TextView textView = frag2Layout.findViewById(R.id.textView);
+            textView.setText(s);
+        }
+    }
+
 }
